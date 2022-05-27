39b5b42c5316eaca2c1f782582aac3b1bc9eda42
Linda <lindathehuang@gmail.com>
2022-05-26 23:49:49 -0700

    Updated file structure and detailed template

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -72,7 +72,7 @@ $(document).ready(function(){
             "background-size":"cover",
             "background-repeat":"no-repeat" 
         });
-        $("#pullupContent .hero h1").html("About Sorrel"); /* Project Name Setup */
+        $("#pullupContent .hero h1").html("About Shroombase"); /* Project Name Setup */
         $("#pullupContent .hero p").html("");
         setPullup(); /* Project Content Setup */
     });
