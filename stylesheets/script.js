b857360b23f4c9e6a47b8dd03f4e4eff2af0959c
Linda <lindathehuang@gmail.com>
2022-05-30 11:47:29 -0700

    Updated fungi

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     checkWidth();
     $(window).resize(function() { checkWidth(); })
     function checkWidth() {
@@ -26,9 +25,9 @@ $(document).ready(function(){
 
 
     // Hero Fix ----------------------------------------------------------------------
-    var conHeight = $("#pullup .hero").height();
-    var imgHeight = $("#pullup .hero img").height();
-    var gap = (imgHeight - conHeight) / 2;
+    let conHeight = $("#pullup .hero").height();
+    let imgHeight = $("#pullup .hero img").height();
+    let gap = (imgHeight - conHeight) / 2;
     $("#pullup .hero img").css("top", -gap);
 
 
